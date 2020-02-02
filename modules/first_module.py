6cbe10d36ea2e770d6fa08e31268f32cdddc7b42
Song Li <da_tou_li@yahoo.com>
2020-02-02 16:07:48 -0500

    Keep playing with Python references and naming scopes

--- modules/first_module.py
+++ modules/first_module.py
@@ -1,7 +1,21 @@
-class FirstClass:
+class _FirstClass:
   def __init__(self, name, age):
     self.name = name
     self.age = age
 
 
-firstObject = FirstClass('Song', 20)
+firstObject = _FirstClass('Song', 20)
+
+class ANumber:
+  def __init__(self, n):
+    self.number = n
+
+a_number = ANumber(50)
+
+def set_a_number(n):
+  a_number.number = n
+
+def get_a_number():
+  return a_number
+
+print('first_module initiated ....')
--- python-test.py
+++ python-test.py
@@ -1,4 +1,4 @@
-from modules.first_module import firstObject
+from modules.first_module import firstObject, set_a_number, get_a_number, a_number, ANumber
 from modules.second_module import another_reference, aInt
 
 print(firstObject.name + ' - ' + str(firstObject.age))
@@ -14,3 +14,12 @@ def aMethod():
 aMethod()
 
 print(aInt)
+
+print('New test ...')
+
+
+a_number = ANumber(200)
+set_a_number(100)
+
+print(get_a_number().number)
+print(a_number.number)
