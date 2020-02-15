8f1f32de59f9b4b2be2ed9d212842385d3d48f1b
Song Li <da_tou_li@yahoo.com>
2020-02-15 11:26:25 -0500

    Play with multi-threading

--- python-flask-api.py
+++ python-flask-api.py
@@ -1,11 +1,13 @@
-import sys, os
+import sys, os, time
+from threading import Thread, current_thread
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def message():
-  return jsonify(pid = os.getpid())
+  time.sleep(10)
+  return jsonify(pid = os.getpid(), thread = current_thread().name)
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=3000)
